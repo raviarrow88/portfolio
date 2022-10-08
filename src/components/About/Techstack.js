a9c401a09ebb51b70ecdf77eaebff82d0026c63b
Bhavik <bhavik.darji@tarrakki.com>
2022-10-08 23:34:21 +0530

    first commit

---
+++ src/components/About/Techstack.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import { CgCPlusPlus } from "react-icons/cg";
+import {
+  DiJavascript1,
+  DiReact,
+  DiPostgresql,
+  DiPython,
+  DiDjango,
+  DiGit,
+} from "react-icons/di";
+import {
+  SiFastapi
+} from "react-icons/si";
+import {FaAws} from 'react-icons/fa'
+function Techstack() {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiPython/>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiDjango/>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiFastapi />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <FaAws/>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiJavascript1/>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiReact />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiPostgresql />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <CgCPlusPlus />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiGit />
+      </Col>
+      
+    </Row>
+  );
+}
+
+export default Techstack;
---
+++ src/components/Home/Home2.js
@@ -0,0 +1,102 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import myImg from "../../Assets/avatar.svg";
+import Tilt from "react-parallax-tilt";
+import {
+  AiFillGithub,
+  AiOutlineTwitter,
+  AiFillInstagram,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+
+function Home2() {
+  return (
+    <Container fluid className="home-about-section" id="about">
+      <Container>
+        <Row>
+          <Col md={8} className="home-about-description">
+            <h1 style={{ fontSize: "2.6em" }}>
+              Who am I?
+            </h1>
+            <p className="home-about-body">
+              I am a experienced software engineer with strong in design and integration with intuitive problem solving skills 
+              <br />
+              <br />I am an expert in classics like
+              <i>
+                <b className="purple"> Python, JavaScript, C, C++, Go </b>
+              </i>
+              <br />
+              <br />
+              My field of Interest's are building &nbsp;
+              <i>
+                <b className="purple">Learning new technologies and building new products </b> and
+                also in areas related to{" "}
+                <b className="purple">
+                  System Design and performance
+                </b>
+              </i>
+              <br />
+              <br />
+            </p>
+          </Col>
+          <Col md={4} className="myAvtar">
+            <Tilt>
+              <img src={myImg} className="img-fluid" alt="avatar" />
+            </Tilt>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12} className="home-about-social">
+            <h1>FIND ME ON</h1>
+            <p>
+              Feel free to <span className="purple">connect </span>with me
+            </p>
+            <ul className="home-about-social-links">
+              <li className="social-icons">
+                <a
+                  href="https://github.com/Bhavikdarji98"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <AiFillGithub />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://twitter.com/Bhavikdrj"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <AiOutlineTwitter />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://in.linkedin.com/in/bhavik-darji-888b69166"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <FaLinkedinIn />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://www.instagram.com/thebhavikdarji"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour home-social-icons"
+                >
+                  <AiFillInstagram />
+                </a>
+              </li>
+            </ul>
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+export default Home2;
