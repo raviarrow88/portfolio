0c5c036acf634caf76d135bf7c0f0f4b46271f8c
raviarrow88 <raviarrow88@gmail.com>
2023-02-05 21:02:09 +0530

    resume js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/raviarrow88/portfolio/master/src/Assets/Ravi_Teja.pdf";
+  "https://raw.githubusercontent.com/raviarrow88/portfolio/master/src/Assets/Ravi_Teja_P_2023.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
